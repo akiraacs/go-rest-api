67a359e66ed5623fda89618a7d533a8c86632902
Adrian Coradini <adrian.coradini3141592@hotmail.com>
2024-10-02 20:59:56 -0300

    Adiciona package de controllers

---
+++ controller/product_controller.go
@@ -0,0 +1,39 @@
+package controller
+
+import (
+	"go-rest-api/model"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type productController struct {
+	//UseCase
+}
+
+func NewProductController() productController {
+	return productController{}
+}
+
+func (p *productController) GetProducts(ctx *gin.Context) {
+
+	products := []model.Product{
+		{
+			ID:    1,
+			Name:  "iPhone 13",
+			Price: 999.99,
+		},
+		{
+			ID:    2,
+			Name:  "iPhone 13 Pro",
+			Price: 1999.99,
+		},
+		{
+			ID:    3,
+			Name:  "iPhone 13 Pro Max",
+			Price: 2999.99,
+		},
+	}
+
+	ctx.JSON(http.StatusOK, products)
+}
